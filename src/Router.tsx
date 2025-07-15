254bf81930d19ef15cbe5474e2df93c6918ab23d
naer <1444736434@qq.com>
2025-07-15 14:34:18 +0800

    fix Router Error

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { lazy } from 'react'
+import { lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import IndexPage from '@/pages/index'
@@ -11,13 +11,15 @@ const AboutPage = lazy(() => import('@/pages/about'))
 
 function Router() {
   return (
-    <Routes>
-      <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
-      <Route element={<PricingPage />} path="/pricing" />
-      <Route element={<BlogPage />} path="/blog" />
-      <Route element={<AboutPage />} path="/about" />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route element={<IndexPage />} path="/" />
+        <Route element={<DocsPage />} path="/docs" />
+        <Route element={<PricingPage />} path="/pricing" />
+        <Route element={<BlogPage />} path="/blog" />
+        <Route element={<AboutPage />} path="/about" />
+      </Routes>
+    </Suspense>
   )
 }
 
--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -1,4 +1,4 @@
-import DefaultLayout from "@/layouts/default";
+import DefaultLayout from '@/layouts/default'
 
 export default function DocsPage() {
   return (
@@ -9,5 +9,5 @@ export default function DocsPage() {
         </div>
       </section>
     </DefaultLayout>
-  );
+  )
 }
